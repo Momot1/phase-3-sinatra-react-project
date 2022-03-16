84c6de94793f1693511b28279958d735330df43e
Mark Momot <markmomot03@gmail.com>
2022-03-15 21:49:30 -0400

    Override Sinatra's not found error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   # Add your routes here
-  
+  error Sinatra::NotFound do
+    {message: "404 not found, please input a valid route"}.to_json
+  end
 end
